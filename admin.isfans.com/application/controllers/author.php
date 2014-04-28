0bf6fca456795a329c25c4275c4ddfc0e2625db5
darren <darren.miao@e-graphics.com>
2014-04-28 17:35:07 +0800

    a

--- admin.isfans.com/application/controllers/author.php
+++ admin.isfans.com/application/controllers/author.php
@@ -4,7 +4,6 @@ class Author extends Admin_Controller{
 		parent::__construct();
 		$this->load->model('author_mdl');
 	}
-	
 	function index(){
 		$data = array();
 		$info = $this->author_mdl->get_user_info_by_id(2);
@@ -14,5 +13,8 @@ class Author extends Admin_Controller{
 		$in = $this->author_mdl->get_cache(2);
 		print_r($in);
 		var_dump($this->cache->cache_info());
+	}
+	function login(){
+		$this->load->view('author/login');
 	}
 }
